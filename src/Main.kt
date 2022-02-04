bcbb1d80cb64028a0d9e36fbfa80ef0136a7da22
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-02-04 09:40:46 +0000

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,19 @@
+class Main {
+
+    fun main(){
+        var check = 0;
+        when(check){
+            1 -> print("you one")
+            2 -> print("you two")
+            3 -> print("you three")
+            4 -> print("you four")
+
+            else -> {
+                System.out.println("You are $check which cannot be touced in real life")
+            }
+
+
+        }
+
+    }
+}
