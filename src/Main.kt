8f00b22d09866c00a2f1072d2fbeec5e70e1d9c6
teamjaf <65031312+teamjaf@users.noreply.github.com>
2022-02-04 09:42:42 +0000

    adding newer way in when

--- src/Main.kt
+++ src/Main.kt
@@ -7,6 +7,8 @@ class Main {
             2 -> print("you two")
             3 -> print("you three")
             4 -> print("you four")
+            in  4..19 -> print("in 4 to 19")
+            in  40 downTo 18 -> print("in 4 to 19")
 
             else -> {
                 System.out.println("You are $check which cannot be touced in real life")
